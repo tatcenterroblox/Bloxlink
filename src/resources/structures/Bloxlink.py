f7279bddd239dbd7d828e86c1098eb490fa0c0bb
xomien <xomien01@gmail.com>
2021-06-10 01:02:57 -0400

    Playing status on startup

--- src/resources/structures/Bloxlink.py
+++ src/resources/structures/Bloxlink.py
@@ -1,8 +1,8 @@
 from importlib import import_module
 from os import environ as env
-from discord import AutoShardedClient, AllowedMentions, Intents
-from config import WEBHOOKS # pylint: disable=E0611
-from ..constants import SHARD_RANGE, CLUSTER_ID, SHARD_COUNT, IS_DOCKER, TABLE_STRUCTURE, RELEASE, SELF_HOST # pylint: disable=import-error
+from discord import AutoShardedClient, AllowedMentions, Intents, Game
+from config import WEBHOOKS, PREFIX # pylint: disable=E0611
+from ..constants import SHARD_RANGE, CLUSTER_ID, SHARD_COUNT, IS_DOCKER, TABLE_STRUCTURE, RELEASE, SELF_HOST, PLAYING_STATUS # pylint: disable=import-error
 from ..secrets import REDIS_PASSWORD, REDIS_PORT, REDIS_HOST, RETHINKDB_HOST, RETHINKDB_DB, RETHINKDB_PASSWORD, RETHINKDB_PORT # pylint: disable=import-error
 from . import Permissions # pylint: disable=import-error
 from async_timeout import timeout
@@ -353,6 +353,7 @@ Bloxlink = BloxlinkStructure(
     shard_ids=SHARD_RANGE,
     allowed_mentions=AllowedMentions(everyone=False, users=True, roles=False),
     intents=intents,
+    activity=Game(PLAYING_STATUS.format(prefix=PREFIX))
 )
 
 
